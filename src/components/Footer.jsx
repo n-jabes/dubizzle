6a94ec6b11deabb195a80c69c18938b15c125027
n-jabes <nshutij7@gmail.com>
2023-01-24 21:59:53 +0200

    finished the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,30 @@ export function Footer() {
   const ShowFooter = () => {
     let footer = footerResources.map((column) => {
       return (
-        <ul key={column.id} className="titles">
-          <li className="title fw-600 d-flex flex-direction-column color-darkgray">
-            <span className="fz-16 fw-600"> {column.title} </span>
-            {column.links.map((link) => {
-              return <a className="link mt-10 cursor-pointer"> {link} </a>;
-            })}
-          </li>
-        </ul>
+        <li
+          key={column.id}
+          className="title fw-600 d-flex flex-direction-column color-darkgray"
+        >
+          <span className="fz-16 fw-600"> {column.title} </span>
+          {column.links.map((link) => {
+            return <a className="link mt-10 cursor-pointer"> {link} </a>;
+          })}
+        </li>
       );
     });
 
     return footer;
   };
   return (
-    <div className="footer ml-10p d-flex justify-content-space-between">
-      <ShowFooter />
+    <div className="footer ml-10p">
+      <ul className="titles d-flex justify-content-space-between">
+        <ShowFooter />
+      </ul>
+
+      <div className="copyright mt-2p ">
+        <div className="logo color-blue fw-600 fz-24">dubizzle</div>
+        <p>&#169; dubizzle-rwanda.com 2023 || All Rights Reserved.</p>
+      </div>
     </div>
   );
 }
